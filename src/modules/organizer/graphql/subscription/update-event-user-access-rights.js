50cc17753fd1fd689ab2fd2e3a6dd442f32c474d
Wolf Utz <w.utz@omega-code.de>
2023-03-05 10:56:35 +0100

    Add subscriptions to lobby room

---
+++ src/modules/organizer/graphql/subscription/update-event-user-access-rights.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+
+export const UPDATE_EVENT_USER_ACCESS_RIGHTS = gql`subscription($eventUserId: ID){
+    updateEventUserAccessRights(eventUserId: $eventUserId){
+        eventId
+        eventUserId
+        verified
+        allowToVote
+        voteAmount
+    }
+}`;
