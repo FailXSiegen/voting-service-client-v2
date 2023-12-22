9a93f61aaa42c2d515818208da5bbed72dc9639c
Wolf Utz <w.utz@omega-code.de>
2023-12-22 13:53:44 +0100

    Fix a bug about event users logging in to different events with the same credentials (record)

--- src/modules/eventUser/views/EventUserFrame.vue
+++ src/modules/eventUser/views/EventUserFrame.vue
@@ -15,12 +15,26 @@ import { onMounted, shallowRef } from "vue";
 import EventUserLogin from "@/modules/eventUser/components/EventUserLogin.vue";
 import EventUserDashboard from "@/modules/eventUser/components/EventUserDashboard.vue";
 import { terminateWebsocketClient } from "@/apollo-client";
+import { logout } from "@/core/auth/login";
+import { toast } from "vue3-toastify";
+import t from "@/core/util/l18n";
 
 // Data.
 
 const coreStore = useCore();
 const event = coreStore.event;
+const eventUser = coreStore.eventUser.value;
 const activeComponent = shallowRef(null);
+
+// Check if event user is in wrong event.
+if (
+  eventUser?.eventId &&
+  parseInt(event.id, 10) !== parseInt(eventUser?.eventId, 10)
+) {
+  logout()
+    .then(() => determineActiveComponent())
+    .then(() => toast(t("notice.logout.wrongEvent"), { type: "info" }));
+}
 
 // Terminate websocket connection, if the user change the tab.
 
