bcd0db74b32b3def8d8ef06c8327e8c2da8cfd56
FailXSiegen <info@failx.de>
2025-05-19 17:15:39 +0200

    add documentation generator with playwright

--- tests/simple-test.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = await page.title();
-  expect(title).toBe('Fast and reliable end-to-end testing for modern web apps | Playwright');
-});
